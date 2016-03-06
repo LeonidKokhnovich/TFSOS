8cd5b22b36db054d56311c9103c1615b77d82e77
Leonid Kokhnovych <leonspeedon@gmail.com>
2016-03-06 11:43:57 -0800

    Add unit tests target.

---
+++ SOS/SOSTests/SOSTests.m
@@ -0,0 +1,39 @@
+//
+//  SOSTests.m
+//  SOSTests
+//
+//  Created by Leonid Kokhnovych on 2016-03-06.
+//  Copyright © 2016 Team14. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SOSTests : XCTestCase
+
+@end
+
+@implementation SOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
